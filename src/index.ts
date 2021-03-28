215b8699e8b817b4cf780869835395294bc60243
Tyler Long <tyler4long@gmail.com>
2021-03-28 16:54:50 -0700

    Fix button text issue

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ const go = new Go();
 
 let mod: WebAssembly.Module, instance: WebAssembly.Instance;
 const button = document.createElement('button');
-button.value = 'Run';
+button.textContent = 'Run';
 button.disabled = true;
 document.body.appendChild(button);
 
